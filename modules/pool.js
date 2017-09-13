3201675a58b5b528baa8cbde7991012aa7419908
Cam <cam.schnackel@gmail.com>
2017-09-13 11:08:22 -0500

    get inventory route connected to db

--- modules/pool.js
+++ modules/pool.js
@@ -7,8 +7,8 @@ var config = {
     port: 5432, // what port is it listening on, 5432 is default
     database: 'canopus-inventory',
     max: 20 // number of clients in pool, default is 10
-}
+};
 
 // pool is an instance of a pool that knows our configuration
 var pool = new Pool(config);
-module.export = pool;
+module.exports = pool;
--- routes/inventory.js
+++ routes/inventory.js
@@ -1,13 +1,37 @@
-var router = require('express').Router()
+var router = require('express').Router();
+var pool = require('../modules/pool');
 
 // global inventory variable
-var inventory = ['the ring', 'trousers', 'buttons', 'hairy feet'];
+// var inventory = ['the ring', 'trousers', 'buttons', 'hairy feet'];
 
 // inventory get route
 // every get needs a send, a send is a response sent from the server to display data
 router.get('/', function (req, res) {
     console.log('in get inventory route');
-    res.send(inventory);
+    pool.connect(function(connectionError, client, done) {
+        // connectionError = connection to db error
+        // client = worker to ask query of
+        // done = function we will call to release client
+        if (connectionError){
+            console.log(connectionError);
+            res.sendStatus(500);
+            // 500 = something BLEW UP
+        } else {
+            // ask the client to run our query
+            // param 1 is query itself, 2 is callback
+            client.query('SELECT * FROM inventory;', function (queryError, resultObj) {
+                if (queryError) {
+                    console.log(queryError);
+                    res.sendStatus(500);
+                } else {
+                    console.log('resultsObject ->', resultObj);
+                    res.send(resultObj.rows);
+                }
+            });
+        }
+
+    });
+//    res.send(inventory);
 })
 
 //inventory post route
